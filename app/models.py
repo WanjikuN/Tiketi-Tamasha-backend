6f88b6faa39cb2828905f4f673790c4100bedf75
kevin <bettkevin965@gmail.com>
2023-11-22 14:08:38 +0300

    Update my models.py and test_models.py files

--- app/models.py
+++ app/models.py
@@ -4,53 +4,30 @@ import re
 
 db = SQLAlchemy()
 
-class Customer(db.Model):
+class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    phone_number = db.Column(db.String(20), nullable=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
 
     @validates('name')
     def validate_name(self, key, name):
         if not name:
-            raise ValueError("Name cannot be empty.")
+            raise ValueError("Role name cannot be empty.")
         return name
 
-    @validates('email')
-    def validate_email(self, key, email):
-        if not email:
-            raise ValueError("Email cannot be empty.")
-        if "@" not in email or "." not in email:
-            raise ValueError("Invalid email address.")
-        return email
-
-    @validates('password')
-    def validate_password(self, key, password):
-        if not password:
-            raise ValueError("Password cannot be empty.")
-        return password
-
-    @validates('phone_number')
-    def validate_phone_number(self, key, phone_number):
-        if phone_number:
-            pattern = re.compile(r'^\+?1?\d{9,15}$|\+?1?\d{3} ?\d{3} ?\d{3}$')
-            if not pattern.match(phone_number):
-                raise ValueError("Invalid phone number format.")
-        return phone_number
-
-class EventOrganizer(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(100), nullable=False, unique=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     phone_number = db.Column(db.String(20), nullable=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)
+    role = db.relationship('Role', backref=db.backref('users', lazy=True))
 
-    @validates('name')
-    def validate_name(self, key, name):
-        if not name:
-            raise ValueError("Name cannot be empty.")
-        return name
+    @validates('username')
+    def validate_username(self, key, username):
+        if not username:
+            raise ValueError("Username cannot be empty.")
+        return username
 
     @validates('email')
     def validate_email(self, key, email):
@@ -79,8 +56,8 @@ class Event(db.Model):
     name = db.Column(db.String(200), nullable=False)
     date = db.Column(db.Date, nullable=False)
     ticket_price = db.Column(db.Numeric(10, 2), nullable=False)
-    organizer_id = db.Column(db.Integer, db.ForeignKey('event_organizer.id'), nullable=False)
-    organizer = db.relationship('EventOrganizer', backref=db.backref('events', lazy=True))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    category = db.relationship('Category', backref=db.backref('events', lazy=True))
 
     @validates('name')
     def validate_name(self, key, name):
@@ -88,11 +65,20 @@ class Event(db.Model):
             raise ValueError("Event name cannot be empty.")
         return name
 
-class CustomerEvent(db.Model):
+class Payment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+    amount = db.Column(db.Numeric(10, 2), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('payments', lazy=True))
     event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
-    tickets_purchased = db.Column(db.Integer, nullable=False, default=0)
+    event = db.relationship('Event', backref=db.backref('payments', lazy=True))
 
-    customer = db.relationship('Customer', backref=db.backref('booked_events', lazy=True))
-    event = db.relationship('Event', backref=db.backref('attendees', lazy=True))
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Category name cannot be empty.")
+        return name
--- app/test_models.py
+++ app/test_models.py
@@ -1,51 +1,63 @@
 from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import db, Customer, EventOrganizer, Event, CustomerEvent
+from models import db, User, Role, Event, Payment, Category
 
 def create_tables():
     engine = create_engine('sqlite:///test.db')
     db.metadata.create_all(engine)
     return engine
 
-def test_customer_model():
+def test_user_model():
     engine = create_tables()
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    new_customer = Customer(name="Maasai Obadia", email="obadiakim@gmail.com", password="obadia123", phone_number="0775649842")
+    new_role = Role(name="Customer")
+    session.add(new_role)
+    session.commit()
+
+    new_customer = User(username="Maasai Obadia", email="obadiakim@gmail.com", password="obadia123", phone_number="0775649842", role_id=new_role.id)
     session.add(new_customer)
     session.commit()
 
-    new_organizer = EventOrganizer(name="Gideon Bett", email="bettgideon456@gmail.com", password="kiptoo", phone_number="0798667046")
+    new_role_organizer = Role(name="Event Organizer")
+    session.add(new_role_organizer)
+    session.commit()
+
+    new_organizer = User(username="Gideon Bett", email="bettgideon456@gmail.com", password="kiptoo", phone_number="0798667046", role_id=new_role_organizer.id)
     session.add(new_organizer)
     session.commit()
 
     event_date = datetime.strptime("2023-11-30", "%Y-%m-%d").date()
 
-    new_event = Event(name="Droidcon", date=event_date, ticket_price=5000.0, organizer_id=new_organizer.id)
+    new_category = Category(name="Tech")
+    session.add(new_category)
+    session.commit()
+
+    new_event = Event(name="Droidcon", date=event_date, ticket_price=5000.0, category_id=new_category.id)
     session.add(new_event)
     session.commit()
 
-    new_customer_event = CustomerEvent(customer_id=new_customer.id, event_id=new_event.id, tickets_purchased=2)
-    session.add(new_customer_event)
+    new_payment = Payment(amount=10000.0, user_id=new_customer.id, event_id=new_event.id)
+    session.add(new_payment)
     session.commit()
 
-    all_customers = session.query(Customer).all()
+    all_customers = session.query(User).filter_by(role_id=new_role.id).all()
     for customer in all_customers:
-        print(customer.name, customer.email, customer.password, customer.phone_number)
+        print(customer.username, customer.email, customer.password, customer.phone_number)
 
-    all_organizers = session.query(EventOrganizer).all()
+    all_organizers = session.query(User).filter_by(role_id=new_role_organizer.id).all()
     for organizer in all_organizers:
-        print(organizer.name, organizer.email, organizer.password, organizer.phone_number)
+        print(organizer.username, organizer.email, organizer.password, organizer.phone_number)
 
     all_events = session.query(Event).all()
     for event in all_events:
-        print(event.name, event.date, event.ticket_price, event.organizer_id)
+        print(event.name, event.date, event.ticket_price, event.category_id)
 
-    all_customer_events = session.query(CustomerEvent).all()
-    for customer_event in all_customer_events:
-        print(customer_event.customer_id, customer_event.event_id, customer_event.tickets_purchased)
+    all_payments = session.query(Payment).all()
+    for payment in all_payments:
+        print(payment.amount, payment.user_id, payment.event_id)
 
 if __name__ == "__main__":
-    test_customer_model()
+    test_user_model()

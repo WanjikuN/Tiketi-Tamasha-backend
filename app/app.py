83eb67848b30de997c6ff4c939a8dfafe586464f
WanjikuN <cikunjoroge4@gmail.com>
2023-11-30 07:53:38 +0300

    update database uri

--- app/app.py
+++ app/app.py
@@ -22,7 +22,7 @@ from datetime import datetime
 import base64
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.secret_key ="12jdhRIF567@#dzv&zhW"
